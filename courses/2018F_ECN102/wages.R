ee752dc188d4c9df18efa6bf0c496c37bd82c6c6
William <wmvolckmann@gmail.com>
2018-11-12 18:20:47 -0800

    OCD marathon

--- courses/2018F_ECN102/wages.R
+++ courses/2018F_ECN102/wages.R
@@ -2,10 +2,10 @@ library("stargazer")
 wages <- read.csv("wages.csv")
 
 reg1 <- lm(wage ~ educ, data = wages)
-stargazer(reg1, type= "text")
+stargazer(reg1, type = "text")
 
 reg2 <- lm(wage ~ educ + IQ, data = wages)
-stargazer(reg2, type= "text")
+stargazer(reg2, type = "text")
 
 reg3 <- lm(wage ~ educ + IQ + urban, data = wages)
-stargazer(reg3, type= "text")
+stargazer(reg3, type = "text")

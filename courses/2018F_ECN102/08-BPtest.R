2c7a3831a6f6ab2c0ecf2122fa3d227ccd41fd32
William <wmvolckmann@gmail.com>
2018-12-09 13:18:20 -0800

    BP test

--- courses/2018F_ECN102/08-BPtest.R
+++ courses/2018F_ECN102/08-BPtest.R
@@ -6,14 +6,14 @@ wages <- read.csv("wages.csv")
 ### unrestricted regression
 reg <- lm(wage ~ educ + IQ + sibs + brthord, data = wages)
 
-uhatsq = reg$residuals^2                    ### square residuals
+esq = reg$residuals^2                       ### square residuals
 
 ### regress squared residuals
-auxreg <- lm(uhatsq ~ educ + IQ + sibs + brthord, data = wages)  
+auxreg <- lm(esq ~ educ + IQ + sibs + brthord, data = wages)  
 
 ### calculate F-statistic and p-value
-Rsquhat = summary(auxreg)$r.squared         ### R-squared for auxiliary
-F  = (Rsquhat/(4)) / ((1 - Rsquhat)/(847))  ### F-statistic
+R2esq = summary(auxreg)$r.squared           ### R-squared for auxiliary
+F  = (R2esq/(4)) / ((1 - R2esq)/(847))      ### F-statistic
 pv = pf(F, 4, 847, lower.tail=FALSE)        ### p-value
 
 ### be lazy and let R give you the p-value
